a8580f603c3454b7d59785cae5bc4dd91fc6bc35
begenov <begenovorazali@icloud.com>
2023-06-29 19:33:28 +0600

    update handler

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -1,1 +1,20 @@
 package http
+
+import (
+	"github.com/begenov/register-service/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
+
+func (h *Handler) Init() *gin.Engine {
+	return nil
+}
