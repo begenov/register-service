9e2c5c16c5a3d7495f2997748ab7a53dc21ab77c
begenov <begenovorazali@icloud.com>
2023-06-29 19:36:12 +0600

    init service

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,10 @@
 package service
+
+import "github.com/begenov/register-service/internal/repository"
+
+type Service struct {
+}
+
+func NewService(repo *repository.Repository) *Service {
+	return &Service{}
+}
