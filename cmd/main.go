c3592605dbb34c0c56763ecc48aeffe1d21baaf4
begenov <begenovorazali@icloud.com>
2023-06-29 18:46:30 +0600

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/begenov/register-service/internal/app"
+	"github.com/begenov/register-service/internal/config"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Error cfg: %v", err)
+	}
+
+	if err = app.Run(cfg); err != nil {
+		log.Fatal("error application\t", err)
+	}
+
+}
