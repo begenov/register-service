51eaa97a74a7fc4a56b79edbf95834f61bb8681a
begenov <begenovorazali@icloud.com>
2023-07-04 21:27:55 +0600

    init courier repository

---
+++ internal/repository/postgres/courier.go
@@ -0,0 +1,60 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/begenov/register-service/internal/domain"
+)
+
+type CourierRepo struct {
+	db *sql.DB
+}
+
+func NewCourierRepo(db *sql.DB) *CourierRepo {
+	return &CourierRepo{
+		db: db,
+	}
+}
+
+func (r *CourierRepo) Create(ctx context.Context, courier domain.Register) error {
+
+	stmt := `INSERT INTO "courier" (email, phone, password_hash) VALUES ($1, $2, $3)`
+	if _, err := r.db.ExecContext(ctx, stmt, courier.Email, courier.Phone, courier.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *CourierRepo) GetCourierByEmail(ctx context.Context, email string) (domain.Register, error) {
+	stmt := `SELECT email, phone, password_hash FROM courier WHERE email = $1`
+	row := r.db.QueryRowContext(ctx, stmt, email)
+	var courier domain.Register
+	if err := row.Scan(&courier.Email, &courier.Phone, &courier.Password); err != nil {
+		return courier, err
+	}
+
+	return courier, nil
+}
+
+func (r *CourierRepo) SetSession(ctx context.Context, token string, expiredAt time.Time, courierId int) error {
+	stmt := `UPDATE courier SET refresh_token = $1, expired_at = $2 WHERE id = $3`
+	if _, err := r.db.ExecContext(ctx, stmt, token, expiredAt, courierId); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *CourierRepo) GetCourierByID(ctx context.Context, id int) (domain.Register, error) {
+	stmt := `SELECT email, phone, password_hash FROM courier WHERE id = $1`
+	row := r.db.QueryRowContext(ctx, stmt, id)
+	var courier domain.Register
+	if err := row.Scan(&courier.Email, &courier.Phone, &courier.Password); err != nil {
+		return courier, err
+	}
+
+	return courier, nil
+}
