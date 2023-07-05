78c0e37edebfdb8d4a5abe3a4b53fae2052e9c8c
begenov <begenovorazali@icloud.com>
2023-07-05 19:57:42 +0600

    update domain

--- internal/domain/register.go
+++ internal/domain/register.go
@@ -3,12 +3,18 @@ package domain
 import "time"
 
 type Register struct {
-	ID       int       `json:"id"`
-	Email    string    `json:"email"`
-	Phone    string    `json:"phone"`
-	Role     string    `json:"role"`
-	Address  string    `json:"address"`
-	Password string    `json:"password"`
-	CreateAt time.Time `json:"create_at"`
-	UpdateAt time.Time `json:"update_at"`
+	ID           int       `json:"id"`
+	Email        string    `json:"email"`
+	Phone        string    `json:"phone"`
+	Role         string    `json:"role"`
+	Address      string    `json:"address"`
+	Password     string    `json:"password"`
+	RefreshToken string    `json:"refresh_token"`
+	ExpiresAt    time.Time `json:"create_at"`
+}
+
+type RequestSignIn struct {
+	Email    string
+	Password string
+	Role     string
 }
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,15 +1,24 @@
 package domain
 
-type Session struct {
+import "time"
+
+type Token struct {
 	AccessToken  string `json:"accesstoken"`
 	RefreshToken string `json:"refreshtoken"`
 }
 
-type Token struct {
-	RefreshToken string `json:"refreshtoken"`
+type Session struct {
+	RefreshToken string    `json:"refreshtoken"`
+	ExpiresAt    time.Time `json:"expires_at"`
 }
 
 type Role struct {
 	UserID int    `json:"userid"`
 	Roles  string `json:"roles"`
 }
+
+var (
+	User       = "user"
+	Courier    = "courier"
+	Restaurant = "restaurant"
+)
