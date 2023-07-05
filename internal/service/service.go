f2973954973289d69d00ecbf73944b80f9f39f4a
begenov <begenovorazali@icloud.com>
2023-07-05 15:01:12 +0600

    init service

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,10 +1,27 @@
 package service
 
-import "github.com/begenov/register-service/internal/repository"
+import (
+	"context"
+	"time"
 
-type Service struct {
+	"github.com/begenov/register-service/internal/domain"
+	"github.com/begenov/register-service/internal/repository"
+	"github.com/begenov/register-service/pkg/auth"
+	"github.com/begenov/register-service/pkg/hash"
+)
+
+type Register interface {
+	SignUp(ctx context.Context, register domain.Register) error
+	SignIn(ctx context.Context, req domain.RequestSignIn) (domain.Token, error)
+	RefreshToken(ctx context.Context, refreshToken string, role string) (domain.Token, error)
 }
 
-func NewService(repo *repository.Repository) *Service {
-	return &Service{}
+type Service struct {
+	Register Register
+}
+
+func NewService(repo *repository.Repository, hash hash.PasswordHasher, auth auth.TokenManager, accessTokenTTL time.Duration, refreshTokenTTL time.Duration) *Service {
+	return &Service{
+		Register: NewRegisterService(repo, hash, auth, accessTokenTTL, refreshTokenTTL),
+	}
 }
